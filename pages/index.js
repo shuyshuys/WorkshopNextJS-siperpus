1a70dd9d3d707f265c656c40ef702a7f280ede4a
Shuya Setsuna <ilvrynsetsuna@protonmail.com>
2023-06-25 22:14:14 +0700

    add search by title and author

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import { collection, getDocs, doc, deleteDoc, query, orderBy } from 'firebase/fi
 
 export default function Home() {
   const [buku, setBuku] = useState([])
+  const [search, setSearch] = useState("")
   const router = useRouter()
 
   const addBookHandler = () => {
@@ -68,36 +69,69 @@ export default function Home() {
           <button onClick={addBookHandler} className='bg-sky-500 text-white px-6 py-2 rounded-full'>
             Tambah Buku
           </button>
+          {/* {Search} */}
+          <div className='flex items-center justify-end'>
+            {/* input */}
+            <div className='relative text-gray-600'>
+              <input
+                title='by title and author'
+                type='search'
+                className='bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none'
+                onChange={(e) => setSearch(e.target.value)}
+                name='search' placeholder='Type to search' />
+              <button type='submit' className='absolute right-3 top-0 mt-3 mr-4'>
+                <svg className='h-4 w-4 fill-current' xmlns='http://www.w3.org/2000/svg' version='1.1' id='Capa_1' x='0px' y='0px' viewBox='0 0 56.966 56.966' width='512px' height='512px'>
+                  <path
+                    d='M55.146,51.075L41.29,37.219c3.479-3.971,5.576-9.013,5.576-14.514C46.866,8.23,38.636,0,28.433,0
+                    C18.23,0,10,8.23,10,18.705c0,10.204,8.23,18.434,18.705,18.434c4.5,0,8.543-2.097,12.514-5.576l13.856,13.856
+                    c0.781,0.781,2.047,0.781,2.828,0l2.828-2.828C55.927,53.122,55.927,51.856,55.146,51.075z M18.705,31.139
+                    c-7.18,0-13.034-5.854-13.034-13.034S11.525,5.07,18.705,5.07s13.034,5.854,13.034,13.034S25.885,31.139,18.705,31.139z'
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
           <div className='mt-5'>
             <table className='table-auto bg-sky-50 py-10 rounded-xl'>
               <thead className='border-b-4'>
                 <tr>
-                  <th className='border px-6 py-2'>Nama Buku</th>
-                  <th className='border px-6 py-2'>Pengarang</th>
-                  <th className='border px-6 py-2'>Deskripsi Buku</th>
-                  <th className='border px-6 py-2'>Tahun Terbit</th>
+                  <th className='border px-6 py-2'>Name of Book</th>
+                  <th className='border px-6 py-2'>Author</th>
+                  <th className='border px-6 py-2'>Description of Book</th>
+                  <th className='border px-6 py-2'>Year of Publication</th>
                   <th className='border px-6 py-2'>Action</th>
                 </tr>
               </thead>
               <tbody>
-                {buku.map((data) => (
-                  <tr className='hover:bg-sky-200' key={data.id}>
-                    <td className='border px-6 py-2'>{data.namaBuku}</td>
-                    <td className='border px-6 py-2'>{data.pengarang}</td>
-                    <td className='border px-6 py-2'>{data.deskripsiBuku}</td>
-                    <td className='border px-6 py-2'>{data.tahunTerbit}</td>
-                    <td className='flex border px-6 py-2'>
-                      <span className='cursor-pointer h-8 w-8 mr-2 hover:text-sky-500'
-                       onClick={() => { updateBookHandler(data.id); }}>
-                        <IkonUbah />
-                      </span>
-                      <span className='cursor-pointer h-8 w-8 mr-2 hover:text-red-500'
-                        onClick={() => {deleteBuku(data.id)}}>
-                        <IkonDelete />
-                      </span>
-                    </td>
-                  </tr>
-                ))}
+                {buku
+                  .filter((data) =>
+                  // data.namaBuku.toLowerCase().includes(search)
+                  {
+                    const searchWords = search.toLowerCase().split(" ");
+                    return searchWords.every((word) =>
+                      data.namaBuku.toLowerCase().includes(word) ||
+                      data.pengarang.toLowerCase().includes(word)
+                    );
+                  }
+                  )
+                  .map((data) => (
+                    <tr className='hover:bg-sky-200' key={data.id}>
+                      <td className='border px-6 py-2'>{data.namaBuku}</td>
+                      <td className='border px-6 py-2'>{data.pengarang}</td>
+                      <td className='border px-6 py-2'>{data.deskripsiBuku}</td>
+                      <td className='border px-6 py-2'>{data.tahunTerbit}</td>
+                      <td className='flex border px-6 py-2'>
+                        <span className='cursor-pointer h-8 w-8 mr-2 hover:text-sky-500'
+                          onClick={() => { updateBookHandler(data.id); }}>
+                          <IkonUbah />
+                        </span>
+                        <span className='cursor-pointer h-8 w-8 mr-2 hover:text-red-500'
+                          onClick={() => { deleteBuku(data.id) }}>
+                          <IkonDelete />
+                        </span>
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           </div>
